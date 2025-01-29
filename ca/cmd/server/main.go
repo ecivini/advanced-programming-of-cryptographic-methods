065602d5838798119ef6e28a1796d7bec6a9fec0
Alessia Pivotto <alessiapivotto1@gmail.com>
2025-01-29 11:32:51 +0100

    initialize HSM and refactor certificate issuance process

--- ca/cmd/server/main.go
+++ ca/cmd/server/main.go
@@ -14,12 +14,23 @@ import (
 	"time"
 )
 
-// GenerateSelfSignedCertificate generates a self-signed certificate and private key for the CA
-func GenerateSelfSignedCertificate() ([]byte, []byte, error) {
+var caPrivateKey *ecdsa.PrivateKey
+
+// initHSM initializes the HSM and generates a private key for the CA
+func initHSM() error {
 	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
+	caPrivateKey = priv
+	return nil
+}
+
+func IssueSignedCertificate() ([]byte, error) {
+	if caPrivateKey == nil {
+		return nil, fmt.Errorf("HSM not initialized")
+	}
+
 	//Create a certificate template, KeyUsage and ExtKeyUsage are required for a CA certificate and BasicConstraintsValid and IsCA must be set to true (anche se a noi non servono teoricamente)
 	template := x509.Certificate{
 		SerialNumber: big.NewInt(1),
@@ -27,44 +38,50 @@ func GenerateSelfSignedCertificate() ([]byte, []byte, error) {
 			Organization: []string{"CA"},
 			CommonName:   "CA",
 		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(1, 0, 0),
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-		IsCA:                  true,
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().AddDate(1, 0, 0),
+		KeyUsage:  x509.KeyUsageCertSign | x509.KeyUsageCRLSign, //Or x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment?
 	}
+
+	userKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
 	//Create the certificate
-	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &userKey.PublicKey, caPrivateKey)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	privBytes, err := x509.MarshalECPrivateKey(priv)
-	if err != nil {
-		log.Fatalf("failed to marshal EC private key: %v", err)
-	}
+	// privBytes, err := x509.MarshalECPrivateKey(priv)
+	// if err != nil {
+	// 	log.Fatalf("failed to marshal EC private key: %v", err)
+	// }
 
-	// Convert to PEM format
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: privBytes})
+	// // Convert to PEM format
+	// certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
+	// keyPEM := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: privBytes})
 
-	return certPEM, keyPEM, nil
+	// return certPEM, keyPEM, nil
+	return pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes}), nil
 }
 
 // HandleIssueCertificate generates a self-signed certificate and private key for the CA and displays the certificate in the browser
 func HandleIssueCertificate(w http.ResponseWriter, r *http.Request) {
-	certPem, _, err := GenerateSelfSignedCertificate()
+	cert, err := IssueSignedCertificate()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain") //If you want to download the certificate as a file, change the content type to "application/x-pem-file"
-	w.Write(certPem)
+	// w.Header().Set("Content-Type", "text/plain") //If you want to download the certificate as a file, change the content type to "application/x-pem-file"
+	// w.Write(certPem)
+	w.Write(cert)
 }
 
 // (Per Ale) Ricorda di usare http e non https
 func main() {
+	if err := initHSM(); err != nil {
+		log.Fatalf("failed to initialize HSM: %v", err)
+	}
+
 	http.HandleFunc("/issue-cert", HandleIssueCertificate)
 
 	fmt.Println("CA Server on port 3000")
